e61eeb6e2deb86cf326947d6d631995f6805e50f
Nguyễn Việt Quang <quangviet932@gmail.com>
2025-10-16 17:31:30 +0700

    Them check point vào phòng

--- Assets/Scripts/ChairCheckPoint.cs
+++ Assets/Scripts/ChairCheckPoint.cs
@@ -3,11 +3,17 @@ using UnityEngine;
 public class ChairCheckPoint : MonoBehaviour
 {
     public GameObject spriteCheckPoint;
-
+    public GameObject player;
     public void Show(bool isShow)
     {
         spriteCheckPoint.gameObject.SetActive(spriteCheckPoint);
     }
 
+    private void LateUpdate()
+    {
+        var direction = player.transform.position - transform.position;
+        direction.Normalize();
+        spriteCheckPoint.transform.forward = direction;
+    }
 
 }
