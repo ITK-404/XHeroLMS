6773835b48c43c81825d9db98b55df041a8700ee
Nguyễn Việt Quang <quangviet932@gmail.com>
2025-10-21 11:39:53 +0700

    Add shader for gradient

--- Assets/Scripts/Book/BookViewUI.cs
+++ Assets/Scripts/Book/BookViewUI.cs
@@ -7,7 +7,39 @@ public class BookViewUI : MonoBehaviour
 
     public Button enterCourseBtn;
     public Button buyCourseBtn;
-
+    public Color leftColor;
+    public Color rightColor;
     public TextMeshProUGUI priceText;
 
+
+    public void Test()
+    {
+        var tmp = priceText;
+        tmp.ForceMeshUpdate();
+        var textInfo = tmp.textInfo;
+
+        for (int i = 0; i < textInfo.characterCount; i++)
+        {
+            if (!textInfo.characterInfo[i].isVisible) continue;
+
+            int vertexIndex = textInfo.characterInfo[i].vertexIndex;
+            int meshIndex = textInfo.characterInfo[i].materialReferenceIndex;
+            var vertices = textInfo.meshInfo[meshIndex].colors32;
+
+            float charPosX = (textInfo.characterInfo[i].bottomLeft.x + textInfo.characterInfo[i].topRight.x) / 2;
+            float t = Mathf.InverseLerp(tmp.bounds.min.x, tmp.bounds.max.x, charPosX);
+            Color32 color = Color32.Lerp(leftColor, rightColor, t);
+
+            vertices[vertexIndex + 0] = color;
+            vertices[vertexIndex + 1] = color;
+            vertices[vertexIndex + 2] = color;
+            vertices[vertexIndex + 3] = color;
+        }
+
+        for (int i = 0; i < tmp.textInfo.meshInfo.Length; i++)
+        {
+            tmp.textInfo.meshInfo[i].mesh.colors32 = tmp.textInfo.meshInfo[i].colors32;
+            tmp.UpdateGeometry(tmp.textInfo.meshInfo[i].mesh, i);
+        }
+    }
 }
