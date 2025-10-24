d89bab3ec7b5a6799cb1846c8d8aee43c9b4c2c2
Khoand <khoand.xhz@xherozone.com>
2025-10-24 12:59:51 +0700

    WIP

--- Assets/Scripts/UI/CourseMenuButtons.cs
+++ Assets/Scripts/UI/CourseMenuButtons.cs
@@ -15,28 +15,32 @@ public class CourseMenuButtons : MonoBehaviour
     // Khóa lưu tạm key giữa các scene
     private const string COURSE_KEY_PREF = "CourseListKey";
 
+    // Hằng số để dùng thống nhất
+    public const string KEY_ALL = "All Courses";
+    public const string KEY_MY  = "My Courses";
+
     void Awake()
     {
         if (btnAllCourses != null)
-            btnAllCourses.onClick.AddListener(() => Go("All Courses"));
+            btnAllCourses.onClick.AddListener(() => Go(KEY_ALL));
 
         if (btnMyCourses != null)
-            btnMyCourses.onClick.AddListener(() => Go("My Courses"));
+            btnMyCourses.onClick.AddListener(() => Go(KEY_MY));
     }
 
     void Go(string key)
     {
-        // Lưu key để scene sau đọc lại
         PlayerPrefs.SetString(COURSE_KEY_PREF, key);
         PlayerPrefs.Save();
 
-        // Chuyển scene
+        // Nếu có transition loader của bạn:
         LoadingTransition.Load(courseSceneName);
+        // hoặc: SceneManager.LoadScene(courseSceneName);
     }
 
     // --- Hàm tiện lợi: để script ở scene đích gọi đọc key ---
     public static string GetSavedKey()
     {
-        return PlayerPrefs.GetString(COURSE_KEY_PREF, "All Courses");
+        return PlayerPrefs.GetString(COURSE_KEY_PREF, KEY_ALL);
     }
 }
