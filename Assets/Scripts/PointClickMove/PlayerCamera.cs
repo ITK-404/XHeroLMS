b643da799dbef42c8c3550dd21b3b9fbdf5c4d46
Nguyễn Việt Quang <quangviet932@gmail.com>
2025-10-27 11:04:37 +0700

    Fix bug

---
+++ Assets/Scripts/PointClickMove/PlayerCamera.cs
@@ -0,0 +1,8 @@
+﻿using Unity.Cinemachine;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    public Camera mainCamera;
+    public CinemachineCamera playerCinemachineCamera;
+}
