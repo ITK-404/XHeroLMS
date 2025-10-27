85d30d79fbcacb45219e4a8acb3c8e293049c44c
Nguyễn Việt Quang <quangviet932@gmail.com>
2025-10-27 15:39:30 +0700

    Fix Logic, add toggle for lesson UI

--- Assets/Scripts/UI/Learn/LearnUI.cs
+++ Assets/Scripts/UI/Learn/LearnUI.cs
@@ -9,16 +9,24 @@ public class LearnUI : MonoBehaviour
     public Button returnBtn;
 
     public Action OnClickReturnBtn;
-
+    public GameObject scrollView;
+    public ToggleBaseUI toggleLessonScrollView;
     private void Awake()
     {
         returnBtn.onClick.AddListener(ClickReturnBtn);
+        toggleLessonScrollView.btn.onClick.AddListener(toggleLessonScrollView.Toggle);
+        toggleLessonScrollView.OnToggleOff.AddListener(() =>{scrollView.gameObject.SetActive(false);});
+        toggleLessonScrollView.OnToggleOn.AddListener(() =>{scrollView.gameObject.SetActive(true);});
         Hide();
     }
-
+    
     private void OnDestroy()
     {
-        returnBtn.onClick.AddListener(ClickReturnBtn);
+        returnBtn.onClick.RemoveListener(ClickReturnBtn);
+        toggleLessonScrollView.btn.onClick.RemoveListener(toggleLessonScrollView.Toggle);
+        toggleLessonScrollView.OnToggleOff.RemoveListener(() =>{scrollView.gameObject.SetActive(false);});
+        toggleLessonScrollView.OnToggleOn.RemoveListener(() =>{scrollView.gameObject.SetActive(true);});
+        
     }
 
     private void ClickReturnBtn()
@@ -30,11 +38,13 @@ public class LearnUI : MonoBehaviour
     {
         Debug.Log("Show Learn UI");
         container.gameObject.SetActive(true);
+        
     }
 
     public void Hide()
     {
         Debug.Log("Hide Learn UI");
         container.gameObject.SetActive(false);
+        toggleLessonScrollView.ChangeState(ToggleBaseUI.State.DeActive);
     }
 }
