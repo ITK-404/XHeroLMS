b872be319a832239c2fc52fb3c0fa7c0ee005f0f
Nguyễn Việt Quang <quangviet932@gmail.com>
2025-10-28 09:25:01 +0700

    Add pausing Video

---
+++ Assets/Scripts/UI/VideoPauseUI.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.UI;
+using static UnityEngine.CullingGroup;
+
+public class VideoPauseUI : MonoBehaviour
+{
+    public VideoPlayerControllerPro videoPlayerControllerPro;
+    public Image background;
+    public Image pauseIcon;
+    private void Awake()
+    {
+        background.gameObject.SetActive(false);
+        pauseIcon.gameObject.SetActive(false);
+        videoPlayerControllerPro.OnPlayStateChanged.AddListener(OnPlayVideoChanged);
+    }
+
+    private void OnDestroy()
+    {
+        videoPlayerControllerPro.OnPlayStateChanged.RemoveListener(OnPlayVideoChanged);
+    }
+
+    private void OnPlayVideoChanged(bool OnStateChanged)
+    {
+        background.gameObject.SetActive(!OnStateChanged);
+        pauseIcon.gameObject.SetActive(!OnStateChanged);
+    }
+}
