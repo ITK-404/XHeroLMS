c4a8d18cb961b5622e60aab1a7a2548be329e96d
Nguyễn Việt Quang <quangviet932@gmail.com>
2025-10-29 17:38:50 +0700

    Add preview video

---
+++ Assets/Scripts/PlayVideoHandleUI.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayVideoHandleUI : MonoBehaviour
+{
+    [SerializeField] PlayVideoOpenBook playVideoOpenBook;
+    [SerializeField] Button skipButton;
+    [SerializeField] RawImage rawImage;
+    [SerializeField] private GameObject container;
+    public Toggle autoSkipToggle;
+    private void Awake()
+    {   
+        container.gameObject.SetActive(false);
+        skipButton.onClick.AddListener(playVideoOpenBook.Stop);   
+    }
+
+    private void OnDestroy()
+    {
+        skipButton.onClick.RemoveListener(playVideoOpenBook.Stop);   
+    }
+
+    public void Show()
+    {
+        container.gameObject.SetActive(true);
+    } 
+
+    public void Hide()
+    {
+        container.gameObject.SetActive(false);
+    } 
+}
