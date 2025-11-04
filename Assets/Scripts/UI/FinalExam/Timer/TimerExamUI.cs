1a8cef3885f0e37d097d5bdb514fa8b86092fc33
Nguyễn Việt Quang <quangviet932@gmail.com>
2025-11-04 14:40:30 +0700

    Add timer UI logic

---
+++ Assets/Scripts/UI/FinalExam/Timer/TimerExamUI.cs
@@ -0,0 +1,16 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class TimerExamUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI timerTMP;
+    [SerializeField] private Color startColor;
+    [SerializeField] private Color finalColor;
+
+    public void UpdateText(string timer,float lerp)
+    {
+        timerTMP.text = timer;
+        timerTMP.color = Color.Lerp(startColor, finalColor, lerp);
+    }
+}
+
