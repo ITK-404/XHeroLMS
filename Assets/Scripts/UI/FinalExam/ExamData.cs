fb11f0ee45a6e1e3f38750c0761e977b3c56ca5a
Nguyễn Việt Quang <quangviet932@gmail.com>
2025-11-04 15:26:24 +0700

    Add exam logic

---
+++ Assets/Scripts/UI/FinalExam/ExamData.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+[Serializable]
+public class ExamData
+{
+    // for testing
+    public string duration = "1800";
+    public string passPointPercent = "90";
+    public string count = "30";
+}
---
+++ Assets/Scripts/UI/FinalExam/ExamManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class ExamManager : MonoBehaviour
+{
+    public ExamData ExamData;
+    public InformationExam InformationExam;
+    public AnswerButtonManager answerButtonManager;
+    public ExamCounter examCounter;
+    private void Awake()
+    {
+        InformationExam.OnStartButtonClick += StartExam;
+    }
+
+    private void OnDestroy()
+    {
+        InformationExam.OnStartButtonClick -= StartExam;
+    }
+
+    private void Start()
+    {
+        ShowExamUI();
+    }
+
+    private void ShowExamUI()
+    {
+        InformationExam.Show();
+        answerButtonManager.Hide();
+        LoadUI();
+    }
+
+    private void StartExam()
+    {
+        answerButtonManager.Show();
+        InformationExam.Hide();
+        examCounter.StartCounter();
+    }
+
+    public void LoadUI()
+    {
+        InformationExam.SetExamData(ExamData);
+        examCounter.SetData(ExamData);
+    }
+}
