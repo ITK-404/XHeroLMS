facab89a7f964c8e9ed1966d9d8927782f29f7f2
Nguyễn Việt Quang <quangviet932@gmail.com>
2025-11-05 14:52:01 +0700

    Add particle and random wind zone

---
+++ Assets/Scripts/RandomWindDirection.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class RandomWindDirection : MonoBehaviour
+{
+    private WindZone windZone;
+
+    [SerializeField] private float timer;
+    [SerializeField]private float minRandomDuration = 1;
+    [SerializeField]private float maxRandomDuration = 60;
+
+    private void Awake()
+    {
+        windZone = GetComponent<WindZone>();
+    }
+
+    private void Update()
+    {
+        if (!windZone)
+        {
+            return;
+        }
+
+        if (timer < 0)
+        {
+            float randomTime = Random.Range(minRandomDuration, maxRandomDuration);
+            float randomY = Random.Range(0, 360);
+            timer = randomTime;
+            windZone.transform.rotation = Quaternion.Euler(0, randomY, 0);
+        }
+        else
+        {
+            timer -= Time.deltaTime;
+        }
+    }
+}
