1bcc25e17f704abc5581fdbad18dbdb249c48728
Nguyễn Việt Quang <quangviet932@gmail.com>
2025-11-06 11:37:18 +0700

    Read offline file

---
+++ Assets/Editor/OpenPersistentDataPath.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class OpenPersistentDataPath
+{
+    [MenuItem("Tools/Open Persistent Data Path")]
+    public static void Open()
+    {
+        var path = Application.persistentDataPath;
+        if (!Directory.Exists(path)) Directory.CreateDirectory(path);
+
+#if UNITY_EDITOR_WIN
+        // Open folder in Windows Explorer
+        Process.Start("explorer.exe", $"\"{path}\"");
+#else
+        // Fallback for macOS / Linux in editor
+        EditorUtility.RevealInFinder(path);
+#endif
+    }
+}
