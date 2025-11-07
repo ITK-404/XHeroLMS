0a3c4712b375b1b6b2320d281bf3e8f452cd22b7
Nguyễn Việt Quang <quangviet932@gmail.com>
2025-11-07 10:38:39 +0700

    Add warning submit UI and result UI

---
+++ Assets/Scripts/UI/FinalExam/ExamInfoElement.cs
@@ -0,0 +1,34 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExamInfoElement : MonoBehaviour
+{
+    [SerializeField] private Sprite unansweredSprite;
+    [SerializeField] private Sprite answeredSprite;
+    [SerializeField] private Image answerImg;
+    [SerializeField] private TextMeshProUGUI coloredTmp;
+    [SerializeField] private TextMeshProUGUI grayTmp;
+    
+    public void SetAnsweredButton()
+    {
+        if (answerImg != null)
+            answerImg.sprite = answeredSprite;
+        coloredTmp.gameObject.SetActive(true);
+        grayTmp.gameObject.SetActive(false);
+    }
+
+    public void SetUnansweredButton()
+    {
+        if (answerImg != null)
+            answerImg.sprite = unansweredSprite;
+        coloredTmp.gameObject.SetActive(false);
+        grayTmp.gameObject.SetActive(true);
+    }
+
+    public void SetQuestionIndexText(string text)
+    {
+        coloredTmp.text = text;
+        grayTmp.text = text;
+    }
+}
---
+++ Assets/Scripts/UI/FinalExam/ExamResultUI.cs
@@ -0,0 +1,42 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExamResultUI : MonoBehaviour
+{
+    public Button continueBtn;
+    public Button checkAnswerBtn;
+
+    public TMP_Text textCorrectAnswer;
+    public TMP_Text textInCorrectAnswer;
+    public TMP_Text skipAnswer;
+    public TMP_Text textTotalPass;
+    public GameObject successGroup;
+    public GameObject unSuccessGroup;
+    public GameObject container;
+    
+    public void Show() => container.gameObject.SetActive(true);
+    public void Hide() => container.gameObject.SetActive(false);
+    
+    public void ShowSuccess()
+    {
+        successGroup.gameObject.SetActive(true);
+        unSuccessGroup.gameObject.SetActive(false);
+    }
+    
+    public void ShowUnSuccess()
+    {
+        successGroup.gameObject.SetActive(false);
+        unSuccessGroup.gameObject.SetActive(true);
+    }
+
+    public void SetTotalAnswerPass(int score,int maxScore)
+    {
+        string color = "#F4A42B";
+        textTotalPass.text =
+            $"BÀI THI CỦA BẠN: <color={color}>{score}/{maxScore}</color>\n" +
+            (score >= 24
+                ? "CHÚC MỪNG! BẠN ĐÃ HOÀN THÀNH XUẤT SẮC!"
+                : "HÃY ÔN LẠI KIẾN THỨC VÀ THỬ LẠI NHÉ!");
+    }
+}
