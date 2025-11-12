8b857fc6dae3477b310dedc5ed67cc425e3a181d
Nguyễn Việt Quang <quangviet932@gmail.com>
2025-11-12 10:01:12 +0700

    Add video logic

---
+++ Assets/Scripts/VideoAPI/VideoContainer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class VideoContainer : MonoBehaviour
+{
+    public RawImage videoContainer;
+    public Transform container;
+
+    public void Show()
+    {
+        container.gameObject.SetActive(true);
+    }
+    
+    public void Hide()
+    {
+        container.gameObject.SetActive(false);
+    }
+}
