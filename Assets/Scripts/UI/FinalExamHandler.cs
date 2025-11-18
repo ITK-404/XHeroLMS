f6e0c2ccc42b7a38f7831d49a36d1e87367ef927
Nguyễn Việt Quang <quangviet932@gmail.com>
2025-11-18 10:08:59 +0700

    Add asset and add camera render 3D Object

--- Assets/Scripts/UI/FinalExamHandler.cs
+++ Assets/Scripts/UI/FinalExamHandler.cs
@@ -23,12 +23,19 @@ public class FinalExamHandler : MonoBehaviour
     private LearnUI learnUI;
 
     private string courseID;
+    public Camera UICamera;
+
     void Awake()
     {
         learnUI = FindAnyObjectByType<LearnUI>();
         videoPlayerControllerPro = FindAnyObjectByType<VideoPlayerControllerPro>();
         playerStandUI = FindAnyObjectByType<PlayerStandUI>();
     }
+    private void Start()
+    {
+        UICamera = PlayerCamera.Instance.playerUICamera;
+    }
+
     void Update()
     {
         if (ExamResultReviewPanel.FlagContinue)
@@ -41,7 +48,6 @@ public class FinalExamHandler : MonoBehaviour
     
     public void SetCourseID(string newCourseID) => courseID = newCourseID;
     private GameObject currentExam;
-    
     public void CreateExamPrefab()
     {
         if (currentExam != null)
@@ -50,6 +56,8 @@ public class FinalExamHandler : MonoBehaviour
             currentExam = null;
         }
         currentExam = Instantiate(examPrefab);
+        var canvas = currentExam.GetComponent<Canvas>();
+        canvas.worldCamera = UICamera;
         examUIController = currentExam.GetComponentInChildren<ExamUIController>();
     }
     
