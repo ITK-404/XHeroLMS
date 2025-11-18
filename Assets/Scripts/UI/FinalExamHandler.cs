41b871d0280762ecf1533ed51e841dfbd6ae67db
Khoand <khoand.xhz@xherozone.com>
2025-11-18 08:49:17 +0700

    Revert "Add matching logic"
    This reverts commit b022784e8631bbc18507a1a63741378fa67fc38e.

--- Assets/Scripts/UI/FinalExamHandler.cs
+++ Assets/Scripts/UI/FinalExamHandler.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using Unity.Cinemachine;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class FinalExamHandler : MonoBehaviour
 {
@@ -41,21 +40,16 @@ public class FinalExamHandler : MonoBehaviour
     }
     
     public void SetCourseID(string newCourseID) => courseID = newCourseID;
-    private ExamUIMock currentExam;
-    public Camera playerCamera;
+    private GameObject currentExam;
+    
     public void CreateExamPrefab()
     {
-        // if (currentExam != null)
-        // {
-        //     Destroy(currentExam.gameObject);
-        //     currentExam = null;
-        // }
-        // currentExam = Instantiate(examPrefab);
-        currentExam = ExamUIMock.Instance;
-        var canvas = currentExam.GetComponent<Canvas>();
-        canvas.worldCamera = playerCamera;
-        canvas.planeDistance = 0.03f;
-        
+        if (currentExam != null)
+        {
+            Destroy(currentExam.gameObject);
+            currentExam = null;
+        }
+        currentExam = Instantiate(examPrefab);
         examUIController = currentExam.GetComponentInChildren<ExamUIController>();
     }
     
@@ -75,13 +69,7 @@ public class FinalExamHandler : MonoBehaviour
 
         if (examCamRoutine != null)
             StopCoroutine(examCamRoutine);
-
-        var operation = SceneManager.LoadSceneAsync("UI_Creator Scene", LoadSceneMode.Additive);
-        operation.completed += OperationOncompleted;
-    }
-
-    private void OperationOncompleted(AsyncOperation obj)
-    {
+        
         CreateExamPrefab();
         examCamRoutine = StartCoroutine(MoveCameraAndOpenExam());
     }
---
+++ Assets/Scripts/UI/MatchingElementUI.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class MatchingElementUI : MonoBehaviour
+{
+    public Transform upperCheckPoint;
+    public Transform bottomCheckPoint;
+
+    private void Awake()
+    {
+        
+    }
+}
