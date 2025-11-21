921548fcfecdc85c0b578d6b2a7e6fa6123dff4f
Nguyễn Việt Quang <quangviet932@gmail.com>
2025-11-21 13:23:03 +0700

    Update Audio mixer

--- Assets/Scripts/Audio/TestAudioBGM.cs
+++ Assets/Scripts/Audio/TestAudioBGM.cs
@@ -11,6 +11,16 @@ public class TestAudioBGM : MonoBehaviour
         btn.onClick.AddListener(ToggleBGMAudio);
     }
 
+    private void Start()
+    {
+        LoadSetting();
+    }
+
+    private void LoadSetting()
+    {
+        isOn = AudioManager.Instance.settings.MusicVolume >= 0.5f;
+    }
+
     private void ToggleBGMAudio()
     {
         isOn = !isOn;
