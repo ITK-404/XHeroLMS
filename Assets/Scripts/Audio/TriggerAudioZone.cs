97bc0d78b8422ae09e0141324e20d050df7817f5
Nguyễn Việt Quang <quangviet932@gmail.com>
2025-11-26 12:32:48 +0700

    Update Audio logic

--- Assets/Scripts/Audio/TriggerAudioZone.cs
+++ Assets/Scripts/Audio/TriggerAudioZone.cs
@@ -1,16 +1,32 @@
+using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class TriggerAudioZone : MonoBehaviour
 {
-    [SerializeField] private AudioClip audioClip;
+    private AudioSource[] audioSources;
+
+    private void Awake()
+    {
+        audioSources = GetComponentsInChildren<AudioSource>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-         
+        // create audio;
+        foreach (var item in audioSources)
+        {
+            item.Play();
+        }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        
+        // delete audio
+        foreach (var item in audioSources)
+        {
+            item.Pause();
+        }
     }
 
 }
