5c8c66b3198108134e0465cde92a6db38c740355
Nguyễn Việt Quang <quangviet932@gmail.com>
2025-11-26 11:51:20 +0700

    Add audio and setup logic

--- Assets/Scripts/Audio/AutoPlayBGM.cs
+++ Assets/Scripts/Audio/AutoPlayBGM.cs
@@ -3,12 +3,6 @@ using UnityEngine;
 
 public class AutoPlayBGM : MonoBehaviour
 {
-    [SerializeField] private AudioClip backgroundMusic;
-    [SerializeField] private int delayTime;
-    private IEnumerator Start()
-    {
-        yield return new WaitForSeconds(delayTime);
-        AudioManager.Instance.PlayMusic(backgroundMusic);
-    }
+    
 
 }
