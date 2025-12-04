fa23fe6d1d81b74660a263a2ae038fd814fe5f60
Nguyễn Việt Quang <quangviet932@gmail.com>
2025-12-04 17:20:50 +0700

    Add block for hover when review exam

--- Assets/Scripts/UI/FinalExam/HoverAnswerButton.cs
+++ Assets/Scripts/UI/FinalExam/HoverAnswerButton.cs
@@ -1,15 +1,22 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 public class HoverAnswerButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     private AnswerButton answerButton;
+    private Button btn;
     private void Awake()
     {
         answerButton = GetComponentInParent<AnswerButton>();
     }
     public void OnPointerEnter(PointerEventData eventData)
     {
+        // component này được gắn vào trong button nên chắc chắn button không null
+        if (GetComponent<Button>().interactable == false)
+        {
+            return;
+        }
         // Bỏ qua khi đã chọn hoặc đang xem lại đáp án
         if (answerButton.isSelect)
         {
