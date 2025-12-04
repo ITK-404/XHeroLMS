10ca1cf7bf208059a402c0d62734b0600071a20e
Nguyễn Việt Quang <quangviet932@gmail.com>
2025-12-04 17:01:47 +0700

    Update Hover button

---
+++ Assets/Scripts/UI/FinalExam/HoverAnswerButton.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class HoverAnswerButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private AnswerButton answerButton;
+    private void Awake()
+    {
+        answerButton = GetComponentInParent<AnswerButton>();
+    }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        // Bỏ qua khi đã chọn hoặc đang xem lại đáp án
+        if (answerButton.isSelect)
+        {
+            return;
+        }
+
+        if (answerButton.IsOnReviewAnswer)
+        {
+            return;
+        }
+
+        answerButton.SetHover(true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (answerButton.isSelect)
+        {
+            return;
+        }
+
+        if (answerButton.IsOnReviewAnswer)
+        {
+            return;
+        }
+        answerButton.SetHover(false);
+    }
+}
