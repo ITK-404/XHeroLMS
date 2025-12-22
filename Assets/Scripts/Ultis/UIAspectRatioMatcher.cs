153340dfa07d29018ceabfe5b0b0dd6d4585cb75
Nguyễn Việt Quang <115866288+GhostZragon@users.noreply.github.com>
2025-12-22 16:24:51 +0700

    Fix order of UI in New Scene, fix input field not show slash

---
+++ Assets/Scripts/Ultis/UIAspectRatioMatcher.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(CanvasScaler))]
+public class UIAspectRatioMatcher : MonoBehaviour
+{
+    private void Awake()
+    {
+        CanvasScaler scaler = GetComponent<CanvasScaler>();
+        
+        // Tính tỉ lệ màn hình hiện tại
+        float aspectRatio = (float)Screen.width / Screen.height;
+
+        // Tỉ lệ thiết kế (Reference Resolution), ví dụ 1920x1080 là ~1.77
+        float targetRatio = scaler.referenceResolution.x / scaler.referenceResolution.y;
+
+        if (aspectRatio < targetRatio)
+        {
+            // Màn hình "vuông" hơn thiết kế (như iPad)
+            // Ưu tiên giữ chiều rộng để không bị mất UI hai bên
+            scaler.matchWidthOrHeight = 0f;
+        }
+        else
+        {
+            // Màn hình "dài" hơn thiết kế (như iPhone X, Sony Xperia)
+            // Ưu tiên giữ chiều cao để không mất UI trên dưới
+            scaler.matchWidthOrHeight = 0.5f;
+        }
+    }
+}
