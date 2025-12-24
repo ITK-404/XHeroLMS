f35806d2b15683daf80ccfc8620bf4a10cf886ac
Khoand <khoand.xhz@xherozone.com>
2025-12-24 14:23:22 +0700

    Update View Certificates

--- Assets/Scripts/UI/FinalExam/ExamResultUI.cs
+++ Assets/Scripts/UI/FinalExam/ExamResultUI.cs
@@ -70,7 +70,7 @@ public class ExamResultUI : MonoBehaviour
             $" \n" +
             (score >= required
                 ? "Chúc mừng quý học viên đã hoàn thành bài thi. Hãy xem kết quả chi tiết ngay"
-                : $"Bạn đã KHÔNG hoàn thành bài thi. BÀI THI CỦA BẠN CẦN ĐẠT TỐI THIỂU {required}/{maxScore} ĐIỂM \n HÃY ÔN LẠI KIẾN THỨC TRONG BÀI HỌC VÀ THỰC HIỆN BÀI THI VÀO LẦN SAU NHÉ !!!;");
+                : $"Bạn đã KHÔNG hoàn thành bài thi. BÀI THI CỦA BẠN CẦN ĐẠT TỐI THIỂU {required}/{maxScore} ĐIỂM \n HÃY ÔN LẠI KIẾN THỨC TRONG BÀI HỌC VÀ THỰC HIỆN BÀI THI VÀO LẦN SAU NHÉ !!!");
 
     }
 }
