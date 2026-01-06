d8f2f978a0839f2b927c31ceb7971e93e3ee8822
Nguyễn Việt Quang <115866288+GhostZragon@users.noreply.github.com>
2026-01-06 16:15:28 +0700

    Setup player marker

---
+++ Assets/Scripts/Minimap/PlayerMarker.cs
@@ -0,0 +1,27 @@
+using System;
+using UnityEngine;
+
+public class PlayerMarker : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    [SerializeField] private RectTransform marker;
+    [SerializeField] private float normalizeValue = 180;
+
+    private void Awake()
+    {
+        marker = GetComponent<RectTransform>();
+    }
+
+    private void LateUpdate()
+    {
+        SyncRotation();
+    }
+
+    private void SyncRotation()
+    {
+        float yRotation = player.transform.eulerAngles.y;
+        yRotation -= normalizeValue;
+        marker.transform.eulerAngles = new Vector3(0, 0, -yRotation);
+        Debug.Log("Y Rotation: "+yRotation);
+    }
+}
