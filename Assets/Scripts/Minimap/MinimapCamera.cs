9abf4febe516840d1488441a48f9699d90ee0822
Nguyễn Việt Quang <115866288+GhostZragon@users.noreply.github.com>
2026-01-06 16:15:28 +0700

    Setup player marker

--- Assets/Scripts/Minimap/MinimapCamera.cs
+++ Assets/Scripts/Minimap/MinimapCamera.cs
@@ -7,8 +7,12 @@ public class MinimapCamera : MonoBehaviour
     [SerializeField] private GameObject camera;
     [SerializeField] private Vector3 offset;
 
+    [SerializeField] private bool canLookAt = false;
+
     private void LateUpdate()
     {
         camera.transform.position = player.transform.position + offset;
+        var direction = player.transform.position - camera.transform.position;
+        camera.transform.forward = direction.normalized;
     }
 }
