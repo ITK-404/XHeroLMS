f731e6e08dfbadbe7d1ea7f2c170bc604f558f9e
Nguyễn Việt Quang <115866288+GhostZragon@users.noreply.github.com>
2026-01-09 18:53:34 +0700

    Update UI

---
+++ Assets/Scripts/Minimap/PlotHandlerUI.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using DG.Tweening;
+using TMPro;
+using UnityEngine;
+
+public class PlotHandlerUI : MonoBehaviour
+{
+    [SerializeField] private GameObject container;
+    [SerializeField] private TextMeshProUGUI bigAreaNameTmp;
+    [SerializeField] private TextMeshProUGUI percentNameTmp;
+
+    public void Show()
+    {
+        container.gameObject.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        container.gameObject.SetActive(false);
+        
+        bigAreaNameTmp.text = "";
+    }
+
+    public void ShowArea(BigArea bigArea)
+    {
+        bigAreaNameTmp.DOKill();
+        bigAreaNameTmp.DOFade(0, 1).OnComplete(() =>
+        {
+            bigAreaNameTmp.text = bigArea.Data.displayName;
+            bigAreaNameTmp.DOFade(1, 1);
+        });
+        UpdatePercentComplete(2, 10);
+    }
+
+    
+
+    private void UpdatePercentComplete(int current, int max)
+    {
+        percentNameTmp.text = $"Đã mở khóa <color=#F9DF99>{current}/{max} ({current/max * 100}%)</color>";
+    }
+}
