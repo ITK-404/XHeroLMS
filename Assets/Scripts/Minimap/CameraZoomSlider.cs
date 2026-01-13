64b921e733ec3262062aced9d0714be24a2a9ab7
Nguyễn Việt Quang <115866288+GhostZragon@users.noreply.github.com>
2026-01-13 15:55:30 +0700

    Add zoom out and zoom in button function

--- Assets/Scripts/Minimap/CameraZoomSlider.cs
+++ Assets/Scripts/Minimap/CameraZoomSlider.cs
@@ -6,7 +6,8 @@ public class CameraZoomSlider : MonoBehaviour
 {
     [SerializeField] private Slider _slider;
     [SerializeField] private GameObject container;
-   
+    [SerializeField] private Button minBtn;
+    [SerializeField] private Button maxBtn;
     public Action<float> OnSliderValueChanged;
     
     private void Awake()
@@ -15,8 +16,16 @@ public class CameraZoomSlider : MonoBehaviour
         {
             _slider.onValueChanged.AddListener(OnValueChanged);
         }
+        
+        minBtn.onClick.AddListener(DecreaseZoom);
+        maxBtn.onClick.AddListener(IncreaseZoom);
         ClampZeroToOne();
     }
+
+    private void IncreaseZoom() => AdjustSlider(0.1f);
+    private void DecreaseZoom() => AdjustSlider(-0.1f);
+
+    private void AdjustSlider(float value) => _slider.value += value;
     
     private void OnDestroy()
     {
@@ -24,6 +33,9 @@ public class CameraZoomSlider : MonoBehaviour
         {
             _slider.onValueChanged.RemoveListener(OnValueChanged);
         }
+        
+        minBtn.onClick.RemoveListener(DecreaseZoom);
+        maxBtn.onClick.RemoveListener(IncreaseZoom);
     }
 
     private void ClampZeroToOne()
