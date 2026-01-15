1634469b29306ee92e48d926769fbcc8f89b4a8e
Nguyễn Việt Quang <115866288+GhostZragon@users.noreply.github.com>
2026-01-15 13:41:30 +0700

    Minimap Update

--- Assets/Scripts/Minimap/ReviewBigAreaUI.cs
+++ Assets/Scripts/Minimap/ReviewBigAreaUI.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,7 +9,7 @@ public class ReviewBigAreaUI : MonoBehaviour
     [SerializeField] private GameObject container;
     [SerializeField] private Button closeBgBtn;
     [SerializeField] private Button closeBtn;
-
+    [SerializeField] private CanvasGroup canvasGroup;
     private void Awake()
     {
         closeBgBtn.onClick.AddListener(Hide);
@@ -26,6 +27,9 @@ public class ReviewBigAreaUI : MonoBehaviour
         reviewInformationTmp.text =
             "Khu vực Lớp học là không gian học tập chuyên sâu, nơi học viên có thể tham gia các lớp học phong thủy từ cơ bản đến nâng cao. Tại đây, học viên được tiếp cận kiến thức chuẩn mực, lộ trình rõ ràng và hướng dẫn trực tiếp từ đội ngũ chuyên gia, giúp nâng cao hiểu biết, ứng dụng phong thủy hiệu quả vào đời sống và công việc.";
         container.gameObject.SetActive(true);
+        
+        canvasGroup.DOFade(0, 0);
+        canvasGroup.DOFade(1, 0.2f);
     }
 
     public void Hide()
