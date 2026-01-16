ec37691c47f7fdecafdc4b3d4b32bf835f394ab5
Nguyễn Việt Quang <115866288+GhostZragon@users.noreply.github.com>
2026-01-16 11:34:26 +0700

    Update InputCanvas.cs

--- Assets/Android/UI/InputCanvas.cs
+++ Assets/Android/UI/InputCanvas.cs
@@ -1,10 +1,27 @@
+using System;
 using UnityEngine;
 
 public class InputCanvas : MonoBehaviour
 {
     public GameObject container;
+    private bool isWindow = false;
+    private void Awake()
+    {
+        isWindow = false;
+        
+        #if UNITY_ANDROID || UNITY_IOS
+        isWindow = true;
+        #endif
+
+        if (isWindow)
+        {
+            Hide();
+        }
+    }
+
     public void Show()
     {
+        if (isWindow) return;
         container.gameObject.SetActive(true);
     }
 
