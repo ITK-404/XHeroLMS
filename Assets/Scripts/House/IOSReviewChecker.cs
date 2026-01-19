4b20d74661c75a8a41094a506a23c9d54386ab39
Nguyễn Việt Quang <115866288+GhostZragon@users.noreply.github.com>
2026-01-19 11:26:27 +0700

    Turn on ios review

--- Assets/Scripts/House/IOSReviewChecker.cs
+++ Assets/Scripts/House/IOSReviewChecker.cs
@@ -6,7 +6,7 @@ public class IOSReviewChecker : MonoBehaviour
     [SerializeField] private List<GameObject> checkerList = new();
     public void Start()
     {
-        bool canShow = false;
+        bool canShow = AppDataGlobal.isInReviewMode == false;
         foreach (var item in checkerList)
         {
             item.gameObject.SetActive(false);
