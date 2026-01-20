de7908d875ae9a6802797944e3b86bb52396d0e0
Nguyễn Việt Quang <115866288+GhostZragon@users.noreply.github.com>
2026-01-20 17:40:52 +0700

    Update StoreUpdateChecker.cs

--- Assets/Scripts/AutoUpdate/StoreUpdateChecker.cs
+++ Assets/Scripts/AutoUpdate/StoreUpdateChecker.cs
@@ -168,10 +168,6 @@ private int TryGetAvailableVersionCode(AppUpdateInfo info)
         return -1;
     }
 }
-#endif
-
-    public void TriggerCheck() => StartCoroutine(CoCheck());
-
     private IEnumerator CoStartAndroidUpdate(AppUpdateInfo info, bool canFlexible, bool canImmediate)
     {
         if (_appUpdateManager == null)
@@ -238,6 +234,11 @@ private int TryGetAvailableVersionCode(AppUpdateInfo info)
             _appUpdateManager.CompleteUpdate();
         }
     }
+#endif
+
+    public void TriggerCheck() => StartCoroutine(CoCheck());
+
+    
 
     private int GetCurrentVersionCodeSafe()
     {
