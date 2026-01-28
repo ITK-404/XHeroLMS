25a7b71a77f7732e618c4e3cec10efa61c3d6671
Nguyễn Việt Quang <115866288+GhostZragon@users.noreply.github.com>
2026-01-28 15:41:05 +0700

    Fix and Improve: Certificates now scale with aspect ratio

---
+++ Assets/Scripts/CertificateScene/AutoSizeCertificates.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class AutoSizeCertificates : MonoBehaviour
+{
+    [SerializeField] private Transform frame;
+    [SerializeField] private Transform certificates;
+    private void Update()
+    {
+        float aspectRatio = (float)Screen.width / Screen.height;
+        
+        if (Mathf.Abs(aspectRatio - 1f) < 0.05f)
+        {
+            transform.localScale = Vector3.one * .7f;
+        }
+        else if (aspectRatio > 1f)
+        {
+            transform.localScale = Vector3.one * 0.6f;
+        }
+
+        certificates.transform.localScale = frame.gameObject.activeSelf ? Vector3.one * 1.8f: Vector3.one * 3.3f;
+    }
+}
