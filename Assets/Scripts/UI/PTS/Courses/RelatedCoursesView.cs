9681f30fa5d0d52a8b3a1c8c09c407b231f16419
Nguyễn Việt Quang <115866288+GhostZragon@users.noreply.github.com>
2026-02-28 10:38:29 +0700

    UI/UX: Adding related course

---
+++ Assets/Scripts/UI/PTS/Courses/RelatedCoursesView.cs
@@ -0,0 +1,30 @@
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RelatedCoursesView : MonoBehaviour
+{
+    [SerializeField] private Button clickCourseBtn;
+    [SerializeField] private TextMeshProUGUI courseTitle;
+    [SerializeField] private TextMeshProUGUI viewCount;
+    [SerializeField] private TextMeshProUGUI customerCount;
+    [SerializeField] string courseID;
+
+    private void Awake()
+    {
+        if(clickCourseBtn)
+            clickCourseBtn.onClick.AddListener(OnShowCourse);
+    }
+
+    private void OnDestroy()
+    {
+        if(clickCourseBtn)
+            clickCourseBtn.onClick.RemoveListener(OnShowCourse);
+    }
+
+    private void OnShowCourse()
+    {
+        // 
+    }
+}
