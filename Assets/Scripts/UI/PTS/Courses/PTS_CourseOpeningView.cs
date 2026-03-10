935a6455f8184e61fd606d4dec892fc58eb9c9eb
Nguyễn Việt Quang <115866288+GhostZragon@users.noreply.github.com>
2026-03-10 14:42:59 +0700

    Update flow of opening class

--- Assets/Scripts/UI/PTS/Courses/PTS_CourseOpeningView.cs
+++ Assets/Scripts/UI/PTS/Courses/PTS_CourseOpeningView.cs
@@ -1,8 +1,18 @@
+using UnityEngine;
+
 public class PTS_CourseOpeningView : PTS_BaseView
 {
+    public static PTS_CourseOpeningView Instance;
+
+    
+    [SerializeField] private FrameEduCourseUI frameEduCourse;
+    [SerializeField] private Transform arrowNavigation;
+    [SerializeField] private CourseDetailInformation courseDetailInformation;
+    [SerializeField] private GameObject coursePanel;
     protected override void Awake()
     {
         base.Awake();
+        Instance = this;
         btnReturn.onClick.AddListener(OnReturn);
     }
 
@@ -16,4 +26,24 @@ public class PTS_CourseOpeningView : PTS_BaseView
         OnEnterNoneView?.Invoke();
         Hide();   
     }
+
+    protected override void OnBeforeShow()
+    {
+        base.OnBeforeShow();
+        frameEduCourse.Show();
+        frameEduCourse.FirstIndex();
+        arrowNavigation.gameObject.SetActive(true);
+        courseDetailInformation.Hide();
+        coursePanel.gameObject.SetActive(false);
+    }
+
+    public void ShowCourseInformation()
+    {
+        // tạm thời
+        coursePanel.gameObject.SetActive(true);
+        frameEduCourse.Hide();
+        var informationType = CourseDetailInformation.InformationType.ContainClass;
+        courseDetailInformation.Show(informationType);
+    }
+
 }
