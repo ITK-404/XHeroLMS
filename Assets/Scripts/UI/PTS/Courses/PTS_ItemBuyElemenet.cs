1a7480662dbf5652ed82ca558e9f7fcdb078e7e5
Nguyễn Việt Quang <115866288+GhostZragon@users.noreply.github.com>
2026-03-12 14:54:29 +0700

    Them giao dien mua do dung

---
+++ Assets/Scripts/UI/PTS/Courses/PTS_ItemBuyElemenet.cs
@@ -0,0 +1,11 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PTS_ItemBuyElemenet : MonoBehaviour
+{
+    [SerializeField] private Toggle _toggle;
+    [SerializeField] private Button buyBtn;
+    [SerializeField] private Image avatarImg;
+    [SerializeField] private TextMeshProUGUI buyItemNameTmp;
+}
