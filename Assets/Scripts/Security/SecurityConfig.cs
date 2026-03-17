77f39d728f79d5f05ee6da7e0d88d6e277592be7
Khoand <khoand.xhz@xherozone.com>
2026-03-17 13:55:57 +0700

    Switch API Prod

--- Assets/Scripts/Security/SecurityConfig.cs
+++ Assets/Scripts/Security/SecurityConfig.cs
@@ -6,8 +6,8 @@ public static class SecurityConfig
 {
     private const string XOR_KEY = "client_side_key_for_obfuscation";
 
-    private const string ENCODED_BASE_URL = "CxgdFR1OcFwIFAwsRgEcKUgXGjodDQcFA00CGwQ="; // api-dev
-    // private const string ENCODED_BASE_URL = "CxgdFR1OcFwIFAwsRgkULEgXGjodDQcFA00CGwQ="; // api-prod
+    // private const string ENCODED_BASE_URL = "CxgdFR1OcFwIFAwsRgEcKUgXGjodDQcFA00CGwQ="; // api-dev
+    private const string ENCODED_BASE_URL = "CxgdFR1OcFwIFAwsRgkULEgXGjodDQcFA00CGwQ="; // api-prod
 
     private static string _cachedBaseUrl;
 
