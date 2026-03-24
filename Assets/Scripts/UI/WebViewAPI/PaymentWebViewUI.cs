b320f68ff9b64289d3c6bcd7bddaf22136a8187b
Nguyễn Việt Quang <115866288+GhostZragon@users.noreply.github.com>
2026-03-24 13:43:05 +0700

    Update payment canvas

---
+++ Assets/Scripts/UI/WebViewAPI/PaymentWebViewUI.cs
@@ -0,0 +1,39 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PaymentWebViewUI : UIView
+{
+    [SerializeField] private Button returnBtn;
+    [SerializeField] private Button actionBtn;
+
+    [SerializeField] private TextMeshProUGUI paymentCompleteTmp;
+    [SerializeField] private TextMeshProUGUI paymentPriceTmp;
+
+    [SerializeField] private Image paymentStateTxtImg;
+    [SerializeField] private Image paymentNotifyImg;
+
+
+    [SerializeField] private Sprite unCompletePayment;
+    [SerializeField] private Sprite completePayment;
+    // Text
+    [SerializeField] private Sprite unCompletePaymentTxt;
+    [SerializeField] private Sprite completePaymentTxt;
+
+    [SerializeField] private string paymentUnCompleteString;
+    [SerializeField] private string paymentCompleteString;
+    public void ShowPayment(bool isComplete, string price)
+    {
+        Show();
+        UpdateByState(isComplete);
+        
+        paymentPriceTmp.text = price;
+        paymentCompleteTmp.text = isComplete ? paymentCompleteString : paymentUnCompleteString;
+    }
+
+    private void UpdateByState(bool isComplete)
+    {
+        paymentStateTxtImg.sprite = isComplete ? completePaymentTxt : unCompletePaymentTxt;
+        paymentNotifyImg.sprite = isComplete ? completePayment : unCompletePayment;
+    }
+}
