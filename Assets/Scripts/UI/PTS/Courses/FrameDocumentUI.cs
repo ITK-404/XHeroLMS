97e3aa3a57c3aa2814342e09cc75a94f8fc3b8f2
Nguyễn Việt Quang <115866288+GhostZragon@users.noreply.github.com>
2026-03-24 09:57:53 +0700

    Improve frame document

--- Assets/Scripts/UI/PTS/Courses/FrameDocumentUI.cs
+++ Assets/Scripts/UI/PTS/Courses/FrameDocumentUI.cs
@@ -32,7 +32,7 @@ public class FrameDocumentUI : PanelBaseUI
     {
         if (string.IsNullOrEmpty(pageUrl))
         {
-            emptyUI.gameObject.SetActive(false);
+            emptyUI.gameObject.SetActive(true);
             return;
         }
 
