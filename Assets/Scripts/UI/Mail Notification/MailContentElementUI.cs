4f77346a7d9088799e2d617ecd9c04663404c7c4
Nguyễn Việt Quang <115866288+GhostZragon@users.noreply.github.com>
2026-04-02 14:54:31 +0700

    Update mail prefab

---
+++ Assets/Scripts/UI/Mail Notification/MailContentElementUI.cs
@@ -0,0 +1,10 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MailContentElementUI : MonoBehaviour
+{
+    [SerializeField] private Image icon;
+    [SerializeField] private TextMeshProUGUI titleTmp;
+    [SerializeField] private TextMeshProUGUI descriptionTmp;
+}
