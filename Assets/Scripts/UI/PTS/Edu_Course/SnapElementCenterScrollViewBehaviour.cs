4aace5595ec19d160cefeae01317c717e23f15b2
Nguyễn Việt Quang <115866288+GhostZragon@users.noreply.github.com>
2026-04-04 09:49:01 +0700

    Update scroll control

---
+++ Assets/Scripts/UI/PTS/Edu_Course/SnapElementCenterScrollViewBehaviour.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SnapElementCenterScrollViewBehaviour : MonoBehaviour, IEndDragHandler, IBeginDragHandler
+{
+    [SerializeField] private SnapElementCenterScrollView customScrollView;
+    [SerializeField] private float stopVelocity = 300f;
+    
+    private bool dragDone = false;
+    private bool isAlign = false;
+    
+    private void OnValidate()
+    {
+        if (customScrollView == null)
+        {
+            customScrollView = GetComponent<SnapElementCenterScrollView>();
+        }
+    }
+   
+    private void Update()
+    {
+        if (customScrollView == null) return;
+        if (dragDone == false)
+        {
+            return;
+        }
+
+        if (customScrollView.GetMagnitude() < stopVelocity && isAlign == false)
+        {
+            customScrollView.AlignCenterOfElement();
+            isAlign = true;
+        }
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        dragDone = true;
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        dragDone = false;
+        isAlign = false;
+    }
+}
