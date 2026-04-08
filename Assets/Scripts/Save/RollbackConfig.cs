79b58c4a2287721536878642ac35c9d902478d3c
Nguyễn Việt Quang <115866288+GhostZragon@users.noreply.github.com>
2026-04-08 09:59:53 +0700

    Refactor editor script to editor folder

--- Assets/Scripts/Save/RollbackConfig.cs
+++ Assets/Scripts/Save/RollbackConfig.cs
@@ -1,7 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using UnityEditor;
 using UnityEngine;
 [CreateAssetMenu(fileName = "Rollback Scene Config",menuName = "Rollback Scene Config")]
 public class RollbackConfig : ScriptableObject
@@ -38,34 +36,3 @@ public class RollbackConfig : ScriptableObject
 
 
 public class SceneDropdownAttribute : PropertyAttribute { }
-
-[CustomPropertyDrawer(typeof(SceneDropdownAttribute))]
-public class SceneDropdownDrawer : PropertyDrawer
-{
-    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
-    {
-        if (property.propertyType != SerializedPropertyType.String)
-        {
-            EditorGUI.HelpBox(position,"Scene dropdown chỉ dùng với string", MessageType.Error);
-            return;
-        }
-
-        string[] scenesName = EditorBuildSettings.scenes.Select(s => System.IO.Path.GetFileNameWithoutExtension(s.path))
-            .ToArray();
-
-        if (scenesName.Length == 0)
-        {
-            EditorGUI.HelpBox(position,"Không có scene nào trong build setting",MessageType.Error);
-            return;
-        }
-
-        int currentIndex = System.Array.IndexOf(scenesName, property.stringValue);
-
-        if (currentIndex < 0) currentIndex = 0;
-
-        EditorGUI.BeginProperty(position, label, property);
-        int selectedIndex = EditorGUI.Popup(position, label.text, currentIndex, scenesName);
-        property.stringValue = scenesName[selectedIndex];
-        EditorGUI.EndProperty();
-    }
-}
