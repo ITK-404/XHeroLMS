4aee53125b28df07bd482b25e20112bf679b0a4f
Nguyễn Việt Quang <115866288+GhostZragon@users.noreply.github.com>
2026-04-08 09:48:50 +0700

    move player position logic to save folder

--- Assets/Scripts/UI/LoadingTransition.cs
+++ Assets/Scripts/UI/LoadingTransition.cs
@@ -1,6 +1,6 @@
 using System.Collections;
 using UnityEngine;
-using UnityEngine.SceneManagement;  
+using UnityEngine.SceneManagement;
 
 #if ADDRESSABLES
 using UnityEngine.AddressableAssets;
@@ -14,6 +14,29 @@ public static class LoadingTransition
     public static string PreviousSceneName;
 
     public static bool UseAddressables;
+    private static SceneNavigationHistory sceneHistory = new (isDebug:false);
+    private static RollbackConfig rollbackConfig;
+    // /// <summary>
+    // /// Dùng để quay về scene trước đó, nếu lịch sử trống thì rollback default scene của scene hiện tại
+    // /// </summary>
+    // /// <param name="currentScene"></param>
+    // /// <returns></returns>
+    // public static IEnumerator RollbackScene(string currentScene)
+    // {
+    //     var sceneToLoad = GetSceneToLoad(currentScene);
+    //
+    // }
+    //
+    // private string GetSceneToLoad(string currentScene)
+    // {
+    //     string sceneToLoad = rollbackConfig.defaultSceneName;
+    //     if (sceneHistory.HasHistory())
+    //     {
+    //         var sceneLocation = sceneHistory.GetPrevious();
+    //     }
+    //
+    //     return sceneToLoad;
+    // }
     /// <summary>
     /// Gọi hàm này để chuyển sang LoadingScene.
     /// LoadingScene sẽ đọc TargetSceneName và load async scene đích.
@@ -27,41 +50,42 @@ public static class LoadingTransition
         SceneManager.LoadScene("LoadingScene", LoadSceneMode.Additive);
     }
 
-private static void LoadAssetBundle(string sceneName)
-{
-    PreviousSceneName = SceneManager.GetActiveScene().name;
-    TargetSceneName = sceneName;
-    UseAddressables = true;
+    private static void LoadAssetBundle(string sceneName)
+    {
+        PreviousSceneName = SceneManager.GetActiveScene().name;
+        TargetSceneName = sceneName;
+        UseAddressables = true;
 
-    Debug.Log($"[LoadingTransition] Load ADDRESSABLE scene --> {sceneName}");
-    SceneManager.LoadScene("LoadingScene", LoadSceneMode.Additive);
-}
+        Debug.Log($"[LoadingTransition] Load ADDRESSABLE scene --> {sceneName}");
+        SceneManager.LoadScene("LoadingScene", LoadSceneMode.Additive);
+    }
 
-public static IEnumerator LoadScene(string targetScene)
-{
-    bool isCloud = false;
-    yield return CoCheckIsCloudScene(targetScene, r => isCloud = r);
+    public static IEnumerator LoadScene(string targetScene)
+    {
+        bool isCloud = false;
+        yield return CoCheckIsCloudScene(targetScene, r => isCloud = r);
 
-    if (isCloud)
-        LoadAssetBundle(targetScene);
-    else
-        Load(targetScene);
-}
+        if (isCloud)
+            LoadAssetBundle(targetScene);
+        else
+            Load(targetScene);
+    }
 
-private static IEnumerator CoCheckIsCloudScene(string sceneKeyOrName, System.Action<bool> result)
-{
-    // var h = Addressables.LoadResourceLocationsAsync(sceneKeyOrName, typeof(SceneInstance));
-    var h = Addressables.LoadResourceLocationsAsync(sceneKeyOrName);
+    private static IEnumerator CoCheckIsCloudScene(string sceneKeyOrName, System.Action<bool> result)
+    {
+        // var h = Addressables.LoadResourceLocationsAsync(sceneKeyOrName, typeof(SceneInstance));
+        var h = Addressables.LoadResourceLocationsAsync(sceneKeyOrName);
 
-    yield return h;
+        yield return h;
 
-    bool ok = (h.Status == AsyncOperationStatus.Succeeded && h.Result != null && h.Result.Count > 0);
+        bool ok = (h.Status == AsyncOperationStatus.Succeeded && h.Result != null && h.Result.Count > 0);
 
-    // Release handle (tránh leak)
-    Addressables.Release(h);
+        // Release handle (tránh leak)
+        Addressables.Release(h);
 
-    result?.Invoke(ok);
-}
+        result?.Invoke(ok);
+    }
+
 
 #if ADDRESSABLES
     public static AsyncOperationHandle<SceneInstance> LoadAddressableAsync()
