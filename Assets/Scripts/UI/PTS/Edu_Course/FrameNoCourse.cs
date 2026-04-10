77e32bd22841f492d7a3ea7d074977b6aa6aceaf
Khoand <khoand.xhz@xherozone.com>
2026-04-10 15:48:15 +0700

    Update FrameNoCourse

--- Assets/Scripts/UI/PTS/Edu_Course/FrameNoCourse.cs
+++ Assets/Scripts/UI/PTS/Edu_Course/FrameNoCourse.cs
@@ -1,4 +1,17 @@
+using TMPro;
+using UnityEngine;
+
 public class FrameNoCourse : PanelBaseUI
 {
-    
+    [SerializeField] private TMP_Text titleText;
+    [SerializeField] private TMP_Text subText;
+
+    public void Setup(string title, string subtext)
+    {
+        if (titleText != null)
+            titleText.text = title ?? "";
+
+        if (subText != null)
+            subText.text = subtext ?? "";
+    }
 }
