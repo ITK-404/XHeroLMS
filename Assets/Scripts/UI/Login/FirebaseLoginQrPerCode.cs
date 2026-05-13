ca9802a130dc7997b93868844bfecdeb7ec148d9
Nguyễn Việt Quang <115866288+GhostZragon@users.noreply.github.com>
2026-05-13 15:02:50 +0700

    Update FirebaseLoginQrPerCode.cs

--- Assets/Scripts/UI/Login/FirebaseLoginQrPerCode.cs
+++ Assets/Scripts/UI/Login/FirebaseLoginQrPerCode.cs
@@ -204,13 +204,14 @@ public class FirebaseLoginQrPerCode : MonoBehaviour
 
         var options = new AppOptions
         {
-            ApiKey = "AIzaSyCw9j5LCQuupiidpQ3nEplzujl7l2LfIc8",
-            AppId = "1:934560089313:web:8edd540216d9dcd6ba63a0",
+            ApiKey = "AIzaSyDbJburhmWdkiH_ed6_oyQHlObe3QJLTOM",
+            AppId = "1:934560089313:ios:a892cbefcba9a279ba63a0",
             ProjectId = "xhero-e1eee",
             DatabaseUrl = new Uri("https://xhero-e1eee-default-rtdb.firebaseio.com"),   
             // DatabaseUrl = new Uri("https://xhero-dev-default-rtdb.firebaseio.com"),
             MessageSenderId = "934560089313",
             StorageBucket = "xhero-e1eee.firebasestorage.app"
+            
         };
 
         try
