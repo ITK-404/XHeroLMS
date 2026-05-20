f8d9a9050a857af2ed672449c20088addb643f06
Nguyễn Việt Quang <115866288+GhostZragon@users.noreply.github.com>
2026-05-20 11:58:09 +0700

    Add custom toggle for audio

---
+++ Assets/Scripts/UI/Setting/ColorToggleSwitch.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorToggleSwitch : ToggleSwitch
+{
+    [Header("Color Transition")]
+    [SerializeField] private Image targetImage;
+    [SerializeField] private Color onColor = Color.green;
+    [SerializeField] private Color offColor = Color.gray;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        transitionEffect = UpdateColor;
+    }
+
+    private void UpdateColor()
+    {
+        targetImage.color = Color.Lerp(offColor, onColor, slider.value);
+    }
+}
