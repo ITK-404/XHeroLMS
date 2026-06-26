817ac47b0d752b1958150c9ffd930b876e5db5ec
Nguyễn Việt Quang <115866288+GhostZragon@users.noreply.github.com>
2026-06-26 10:37:15 +0700

    Fix user can click thourgh ui to book

--- Assets/Scripts/Book/BookShelfUI.cs
+++ Assets/Scripts/Book/BookShelfUI.cs
@@ -1,9 +1,36 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 public class BookShelfUI : MonoBehaviour
 {
     [HideInInspector] public BookHandler[] books ;
+    private RectTransform boundRect;
     private void Awake()
     {
         books = GetComponentsInChildren<BookHandler>();
+    }
+
+    private void Start()
+    {
+        foreach (var bookHandler in books)
+        {
+            var bookModel = bookHandler.bookModel;
+            bookModel.CanPlayerClick = CanPlayerClickBook;
+        }
+    }
+
+    public void SetClickBounds(RectTransform bounds) => boundRect = bounds;
+
+    private bool CanPlayerClickBook() => TryClick(Input.mousePosition);
+
+    private bool TryClick(Vector2 screenPosition)
+    {
+        if (boundRect == null)
+            return true;
+
+        return RectTransformUtility.RectangleContainsScreenPoint(
+            boundRect,
+            screenPosition,
+            Camera.main// hoặc Camera.main nếu Canvas là Screen Space - Camera
+        );
     }
 }
