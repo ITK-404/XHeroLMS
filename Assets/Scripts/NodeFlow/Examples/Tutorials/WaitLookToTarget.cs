d81cad2c303e50fe21bb104f4321dd5d8910642b
Nguyễn Việt Quang <115866288+GhostZragon@users.noreply.github.com>
2026-07-23 16:08:24 +0700

    Handle look took to target

---
+++ Assets/Scripts/NodeFlow/Examples/Tutorials/WaitLookToTarget.cs
@@ -0,0 +1,35 @@
+using DG.Tweening;
+using UnityEngine;
+
+public class WaitLookToTarget : TutorialStepBehaviour
+{
+    [SerializeField] private Transform target;
+
+    private PointClickSystem pointClickSystem;
+    private bool isDone = false;
+
+    public override void Enter()
+    {
+        base.Enter();
+        pointClickSystem = FindFirstObjectByType<PointClickSystem>();
+        RotateToPoint();
+    }
+    [ContextMenu("RotateToPoint")]
+    private void RotateToPoint()
+    {
+        Debug.Log("[ClickCheckPointStep] rorate to check point");
+        var direction = target.transform.position - pointClickSystem.transform.position;
+        direction.y = 0;
+        direction.Normalize();
+        
+        var targetRotation = Quaternion.LookRotation(direction);
+        pointClickSystem.transform.DORotateQuaternion(targetRotation, 3f).OnComplete(() =>
+        {
+            isDone = true;
+        });
+    }
+    public override bool IsCompleted()
+    {
+        return isDone;
+    }
+}
