51e4fe27232bb9f317dff7b99bee27d21011e128
Nguyễn Việt Quang <115866288+GhostZragon@users.noreply.github.com>
2026-07-27 09:29:25 +0700

    Improve game flow

--- Assets/Scripts/NodeFlow/Examples/ClassTutorialFlow.cs
+++ Assets/Scripts/NodeFlow/Examples/ClassTutorialFlow.cs
@@ -37,7 +37,7 @@ public class ClassTutorialFlow : FlowBase
 
     private async UniTask HandleFlow()
     {
-        GameplayLock.Lock(GameplayLockReason.Animation, GameplayLockTarget.Movement);
+        // GameplayLock.Lock(GameplayLockReason.Animation, GameplayLockTarget.Movement);
         await UniTask.WaitForSeconds(2f);
 
         ShowBlockPanel(true);
